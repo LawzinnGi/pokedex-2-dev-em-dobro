969c5caef8a893d9f0236f7e0ea4181e8795f1f7
LawzinnGi <133606504+LawzinnGi@users.noreply.github.com>
2023-05-14 18:32:26 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+const botaAlterarTema= document.getElementById("botao-alterar-tema"); 
+
+const body=document.querySelector("body");
+
+const imagemBotaoTrocaDeTema 
+=document.querySelector(".imagem-botao")
+
+
+botaAlterarTema.addEventListener("click", () =>{
+
+    const modoEscuroEstaativo= body.classList.contains("modo-escuro");
+
+    
+
+    body.classList.toggle("modo-escuro");
+
+    
+    if (modoEscuroEstaativo) {imagemBotaoTrocaDeTema.setAttribute("src" , "./src/imagens/sun.png")
+    } 
+    else{
+        body.classList.add("modo-escuro");
+        
+        
+        
+        imagemBotaoTrocaDeTema.setAttribute("src" , "./src/imagens/moon.png")
+
+    }
+
+
+
+ 
+})
